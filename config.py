537f63fe81a68b265c3079ecc1f663bd483d6b60
Devil-Eyes73 <devileyes649@gmail.com>
2025-10-23 22:52:37 +0530

    Update configuration values in config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "25800540")
+    API_HASH  = os.environ.get("API_HASH", "ca9ddda8bb0ac3cf4f2e2b10c434e538")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7598481951:AAFj-bRvmCfJL2nJrG__FCkr3upjowRp4Uc") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","Cluster0")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://devileyes4252:CIlEzvNuwJXhXvxH@cluster0.afoic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    START_PIC   = os.environ.get("START_PIC", "https://i.ibb.co/cc6xpTmj/x.jpg")
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5636597174').split()]
 
     # channels logs
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "Code_Wolf_Xe") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002107659375"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
